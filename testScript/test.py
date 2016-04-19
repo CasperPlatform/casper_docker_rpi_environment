b010e1e49b51715f75875aba569c4a47a52114d2
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-19 22:54:48 +0200

    Mod script

--- testScript/test.py
+++ testScript/test.py
@@ -1,3 +1,13 @@
 from pymongo import MongoClient
+import datetime
 client = MongoClient('localhost', 10080)
 db = client.test_database
+
+post = {"author": "Mike",
+       "text": "My first blog post!",
+       "tags": ["mongodb", "python", "pymongo"],
+       "date": datetime.datetime.utcnow()}
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print post_id
+print posts.find_one()       
