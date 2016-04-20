d1dc1167764f8a252bc2d2ae0924ed41027be2f6
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-20 22:35:53 +0200

    Refact

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
-echo "mounting volume: "$VOLUME
+echo "mounting volume: "$VOLUME"/data
+DATAVOL=$VOLUME"/data"
 
 docker run -d -p 10080:27017 --name "mongodb" \
--v $VOLUME/data:/data/db  \
+-v $DATAVOL:/data/db  \
 group_one/mongodb; 
