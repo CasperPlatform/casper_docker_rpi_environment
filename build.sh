65a129497eda9b2d8ea17d1f876e79727c0003d5
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-20 18:24:41 +0200

    Refact

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set e
 cd $(dirname $0)
 
 DIR=$(basename $1)
-TAG = casper
-VOLUME = $("/home/data/casper/"$TAG"-"$DIR"/data")
+TAG=casper
+VOLUME=$("/home/data/casper/"$TAG"-"$DIR"/data")
 echo $VOLUME
 
 cd $DIR
