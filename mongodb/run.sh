670db319df8b7d816d4d30e9d94b786ed38fa1ed
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-20 22:51:42 +0200

    Refact

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -2,6 +2,7 @@
 echo "mounting volume: "$VOLUME"/data"
 DATAVOL=$VOLUME"/data"
 
+
 docker run -d -p 10080:27017 --name "mongodb" \
--v $DATAVOL:/data/db  \
+-v /home/data/casper/casper-mongodb/data:/data/db  \
 group_one/mongodb; 
