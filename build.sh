4fdf833e095b4a94885aeb681f346515b3b7d618
data <pontus.pohl@gmail.com>
2016-04-20 16:16:42 +0000

    mmsg

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set e
+
 cd $(dirname $0)
 
 DIR=$(basename $1)
+TAG = casper
+VOLUME = $(/home/data/casper/$TAG - $DIR/data)
+echo $VOLUME
 
-echo $DIR
+cd $DIR
+
+mkdir -p $VOLUME
 
 #docker build --rm -t group_one/mongodb ./build/;
