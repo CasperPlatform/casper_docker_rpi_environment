b78a805b179fb7cfc929ac503c1ead15fb7b45af
Pontus Pohl <pontus.pohl@gmail.com>
2016-04-24 20:30:32 +0200

    update

--- demo_mongo_client/src/app.js
+++ demo_mongo_client/src/app.js
@@ -7,7 +7,40 @@ var url = 'mongodb://127.0.0.1:10080/casper';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
+  insertDocuments(db, function() {
+      findDocuments(db, function(res){
+         
+         db.close(); 
+      });
  
-  db.close();
+  });  
+  
 });  
   
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('Users');
+  // Insert some documents 
+  collection.insertMany([
+    {name : "casper",pass : "casper", token : "32432634823##"}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}  
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Find some documents 
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    //assert.equal(2, docs.length);
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
