02c7a407863d2c5933f37a41588be1b36fd1aa2f
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-09 20:39:32 +0200

    modded build

--- ros/protosrc/protobuild.sh
+++ ros/protosrc/protobuild.sh
@@ -25,7 +25,7 @@ SRCFILES="$SRCDIR/rosproto.cpp"
 
 
 COMMAND="g++ $SRCFILES -o build/hello_world_node \
--I/opt/ros/indigo/include -L/opt/ros/indigo/lib -Wl,-rpath,/opt/ros/indigo/lib\
+-I/opt/ros/indigo/include -L/opt/ros/indigo/lib -Wl,-rpath,/opt/ros/indigo/lib \
 -lroscpp -lrosconsole -lrostime"
 
 #COMMAND="gcc -o build/sens src/mqttClient.c \
