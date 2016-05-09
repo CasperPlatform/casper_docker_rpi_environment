f89d931b2e82afcca7f169073b358503a7573cc1
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-09 18:20:06 +0200

    modded build

--- ros/run.sh
+++ ros/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker stop rosrpitest && docker rm rosrpitest
-docker run -d --name -it rosrpitest casper/ros bash
+docker run -d --name rosrpitest -it casper/ros bash
 docker exec -it --user root rosrpitest bash
