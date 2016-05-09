20a5ad75732d7ecafe8cccf08812bbadacdef542
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-09 18:27:47 +0200

    modded build

--- ros/run.sh
+++ ros/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SRC="${PWD}"
+VOL="$SRC/protosrc"
+MOUNT="/src"
+
 docker stop rosrpitest && docker rm rosrpitest
-docker run -d --name rosrpitest -it casper/ros bash
+docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros bash
 docker exec -it --user root rosrpitest bash
