dc81456206c32e0e3f4b3a6e203025dbfddc37b5
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-10 22:05:23 +0200

    Refact build stuff

--- ros/run.sh
+++ ros/run.sh
@@ -10,10 +10,12 @@ if [ "$#" == 0 ]; then
 	echo "no params, cmd=bash"
 	PAR=bash
 else
+	echo "par is $1"
 	PAR=$1
 fi
 
 docker stop rosrpitest && docker rm rosrpitest
 docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
-docker exec -it --user root rosrpitest bash
+
+#docker exec -it --user root rosrpitest bash
