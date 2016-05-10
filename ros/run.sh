d8232752923d9f4707f724215d4f18420439bc56
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-10 21:43:00 +0200

    Add makefile

--- ros/run.sh
+++ ros/run.sh
@@ -6,7 +6,13 @@ BASEDIR="${PWD}"
 VOL="$BASEDIR/protosrc/"
 MOUNT="/src/ros"
 
+if [ "$#" == 0 ]; then 
+	PAR=bash
+else
+	PAR=$1
+fi
+
 docker stop rosrpitest && docker rm rosrpitest
-docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros 
+docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
 docker exec -it --user root rosrpitest bash
