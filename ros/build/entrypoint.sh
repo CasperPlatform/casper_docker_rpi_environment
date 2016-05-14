7671dde8667cc975bda4a820ee9c4bd8c4360d22
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-14 16:52:43 +0200

    also building driveserver now

--- ros/build/entrypoint.sh
+++ ros/build/entrypoint.sh
@@ -5,11 +5,12 @@ set -x
 INDIGO=/opt/ros/indigo
 SRC=/src/ros
 LIDARSRC=$SRC/lidarserver
+DRIVESRC=$SRC/driveserver
 cd $INDIGO && source setup.sh
 
 if [ "$1" == "buildrun" ]; then
 	
-	echo "building.." >> /dev/stdout 
+	echo "building..lidarserver" >> /dev/stdout 
 	cd $LIDARSRC && make
 	if [ ! -f $LIDARSRC/build/lidarserver ]; then
 		echo "build failed..." 2>&1
@@ -17,7 +18,15 @@ if [ "$1" == "buildrun" ]; then
 	fi	
 	exec roscore &
 	sleep 8
-	exec $LIDARSRC/build/lidarserver
+	exec $LIDARSRC/build/lidarserver &
+	echo "Building driveserver" >> /dev/stdout
+	cd $DRIVESRC && make
+	if [ ! -f $DRIVESRC/build/driveserver ]; then
+		echo "build failed..." 2>&1
+		exit 1
+	fi	
+	exec $DRIVESRC/build/driveserver
+
 elif [ "$1" == "run" ]; then
 	if [ ! -f $LIDARSRC/build/lidarserver ]; then
 		echo "no executable present..." 2>&1
