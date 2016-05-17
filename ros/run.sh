b18e0f0570c565868f6631613f30a89308085027
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-17 20:21:38 +0200

    Added -v flag

--- ros/run.sh
+++ ros/run.sh
@@ -17,7 +17,7 @@ fi
 docker stop rosrpitest && docker rm rosrpitest
 echo "running: docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR"
 #docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $
-docker run -d --name rosrpitest -it casper/ros $PAR
+docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
 
 docker exec -it --user root rosrpitest bash
