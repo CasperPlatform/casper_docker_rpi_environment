db86f61a91633be3b22bed971e97b631406fd142
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-17 22:53:26 +0200

    Add device mount

--- ros/run.sh
+++ ros/run.sh
@@ -13,11 +13,14 @@ else
 	echo "par is $1"
 	PAR=$1
 fi
+USBDIR=/dev/ttyAMA0
+USBMOUNT=/dev/ttyUSB0
 
 docker stop rosrpitest && docker rm rosrpitest
 echo "running: docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR"
 #docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $
-docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR
+docker run -d --name rosrpitest -it -v $VOL:$MOUNT --device=$USBDIR:$USBMOUNT \
+casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
 
 docker exec -it --user root rosrpitest bash
--- ros_rpi_emu/run.sh
+++ ros_rpi_emu/run.sh
@@ -8,10 +8,12 @@ QEMUMOUNT=/usr/bin/qemu-arm-static
 
 SRCDIR=$1
 SRCMOUNT=/root/catkin_ws/src
-
+USBDIR=$2
+USBMOUNT=/dev/ttyUSB0
 
 echo "Starting container and spawning a shell"
 docker pull corroleaus/casper-ros:beta
 docker stop rpi_ros_emulator && docker rm rpi_ros_emulator
 docker run --name rpi_ros_emulator \
--v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT -it corroleaus/casper-ros:beta
+-v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT --device=$USBDIR:$USBMOUNT \
+-it corroleaus/casper-ros:beta
