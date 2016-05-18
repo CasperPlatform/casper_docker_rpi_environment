69fba1e277f77ab24aaa41eb9c5435107b46659c
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-18 17:14:23 +0200

    fix

--- ros/run.sh
+++ ros/run.sh
@@ -16,11 +16,14 @@ fi
 USBDIR=/dev/ttyUSB0
 USBMOUNT=/dev/ttyUSB0
 
+DBDIR=/home/data/casper/db.db
+DBMOUNT=/root/db.db
+
 docker stop rosrpitest && docker rm rosrpitest
 echo "running: docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $PAR"
 #docker run -d --name rosrpitest -it -v $VOL:$MOUNT casper/ros $
 docker run -d --name rosrpitest -it -v $VOL:$MOUNT --device=$USBDIR:$USBMOUNT \
--p 9999:9999 -p 6000:6000 -p 9998:9998 casper/ros $PAR
+-v $DBDIR:$DBMOUNT -p 9999:9999 -p 6000:6000 -p 9998:9998  casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
 
 docker exec -it --user root rosrpitest bash
