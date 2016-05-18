d4930cefe1c2f6a8c6aa067f3b6e065f534abecf
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-18 15:54:42 +0200

    Fixes

--- ros_rpi_emu/run.sh
+++ ros_rpi_emu/run.sh
@@ -17,11 +17,13 @@ echo "$ARCH"
 echo "Starting container and spawning a shell"
 docker pull corroleaus/casper-ros:beta
 docker stop rpi_ros_emulator && docker rm rpi_ros_emulator
-if [ ! "$ARCH" == "Darwin" || "$#" == 2 ]; then
+if [ "$ARCH" != "Darwin" ] || [ "$#" == 2 ] ; then
+	echo "Running with device-flag"
 	docker run --name rpi_ros_emulator \
 	-v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT --device=$USBDIR:$USBMOUNT \
 	-it corroleaus/casper-ros:beta
 else
+	echo "Running without device-flag"
 	docker run --name rpi_ros_emulator \
 	-v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT \
 	-it corroleaus/casper-ros:beta
