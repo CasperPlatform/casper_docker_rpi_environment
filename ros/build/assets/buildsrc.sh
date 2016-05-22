7f273212cd8bdd3c0c04580b2ea452ce580febe9
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-22 22:20:09 +0200

    fix

--- ros/build/assets/buildsrc.sh
+++ ros/build/assets/buildsrc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -I
 
-source /opt/ros/indigo/setup.bash && source /envi.sh \
-&& cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver 
 python /home/builder/src/restful/server/restful.py &
+source /opt/ros/indigo/setup.bash && source /envi.sh \
+&& cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver &&
+source devel/setup.bash \
+&& rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver
+
 
