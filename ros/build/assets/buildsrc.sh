56b8273f042ee43d69e6ecce486a1c621c7ec0fa
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-23 18:10:53 +0200

    Don't run at start

--- ros/build/assets/buildsrc.sh
+++ ros/build/assets/buildsrc.sh
@@ -4,6 +4,6 @@ python /home/builder/src/restful/server/restful.py &
 source /opt/ros/indigo/setup.bash && source /envi.sh \
 && cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver &&
 source devel/setup.bash \
-&& rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver
-
+#&& rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver
+#&& rosrun 
 
