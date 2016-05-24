996aa3b58fc70cd3368b1d55c2b03a02200134aa
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-24 11:11:28 +0200

    roslaunch hector slam

--- ros/build/assets/buildsrc.sh
+++ ros/build/assets/buildsrc.sh
@@ -2,8 +2,10 @@
 
 python /home/builder/src/restful/server/restful.py &
 source /opt/ros/indigo/setup.bash && source /envi.sh \
+&& roslaunch hector_slam_launch tutorial.launch
+
 && cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver &&
 source devel/setup.bash \
-#&& rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver
-#&& rosrun 
+
+&& rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver
 
