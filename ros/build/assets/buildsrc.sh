51befeaedaeb0a11b76607f863d1888520c2d789
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-24 16:13:26 +0200

    testing,

--- ros/build/assets/buildsrc.sh
+++ ros/build/assets/buildsrc.sh
@@ -4,9 +4,9 @@ python /home/builder/src/restful/server/restful.py &
 source /opt/ros/indigo/setup.bash \
 && roslaunch hector_mapping mapping_default.launch &
 
-source /envi.sh 
-
-&& cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver &&
-source devel/setup.bash \
+source /opt/ros/indigo/setup.bash \
+&& source /envi.sh \
+&& cd /home/builder/catkin_ws && catkin_make --pkg casper_lidar_scanner_driver \
+&& source devel/setup.bash \
 && rosrun casper_lidar_scanner_driver casper_lidar_scanner_driver &
 
