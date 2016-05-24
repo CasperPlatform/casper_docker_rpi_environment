a31c00d6882b292aa37c17210ee47f1d19a5055b
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-24 18:15:15 +0200

    fix

--- ros/run.sh
+++ ros/run.sh
@@ -32,9 +32,9 @@ echo "running: docker run -d --name rosrpitest -it -v $VOL:$MOUNT -v $RESTVOL:$R
 docker run -d --name rosrpitest -it -v $VOL:$MOUNT -v $RESTVOL:$RESTMOUNT -v $CPPVOL:$CPPMOUNT --device=$USBDIR:$USBMOUNT \
 -v $DBDIR:$DBMOUNT -p 9999:9999/udp -p 6000:6000/udp -p 9998:9998/udp -p 10000:10000 casper/ros $PAR
 #docker exec --user root rosrpitest /bin/sh -c "source /opt/ros/indigo/setup.sh;"
-docker exec -it --user builder rosrpitest bash -v /buildLidar.sh
 docker exec -it --user builder rosrpitest bash -v /runRest.sh
 docker exec -it --user builder rosrpitest bash -v /runSlam.sh
+docker exec -it --user builder rosrpitest bash -v /buildLidar.sh
 docker exec -it --user builder rosrpitest bash -v /runVideo.sh
 docker exec -it --user builder rosrpitest bash
 
--- ros_rpi_emu/run.sh
+++ ros_rpi_emu/run.sh
@@ -3,11 +3,21 @@
 cd $(dirname $0)
 BASEDIR="${PWD}"
 
+SYSDEV=/Users/pontus/Documents/coding/sem/systemDev
+
 QEMUVOL=$BASEDIR/assets/qemu-arm-static
 QEMUMOUNT=/usr/bin/qemu-arm-static
 
-SRCDIR=$1
-SRCMOUNT=/root/catkin_ws/src
+VOL=$SYSDEV/c++/p0015/
+RESTVOL="$SYSDEV/python/p0006"
+CPPVOL=$SYSDEV/c++/p0010
+MOUNT=/home/builder/catkin_ws/src
+RESTMOUNT=/home/builder/src/restful
+CPPMOUNT=/home/builder/src/cpp
+
+DBDIR=$SYSDEV/db.db
+DBMOUNT=/home/builder/db.db
+
 USBDIR=$2
 USBMOUNT=/dev/ttyUSB0
 
@@ -15,16 +25,20 @@ ARCH=$(uname)
 echo "$ARCH"
 
 echo "Starting container and spawning a shell"
-docker pull corroleaus/casper-ros:beta
+#docker pull corroleaus/casper-ros:beta
 docker stop rpi_ros_emulator && docker rm rpi_ros_emulator
 if [ "$ARCH" != "Darwin" ] || [ "$#" == 2 ] ; then
 	echo "Running with device-flag"
 	docker run --name rpi_ros_emulator \
 	-v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT --device=$USBDIR:$USBMOUNT \
-	-it corroleaus/casper-ros:beta
+	-it corroleaus/casper-ros:alpha
 else
 	echo "Running without device-flag"
 	docker run --name rpi_ros_emulator \
-	-v $QEMUVOL:$QEMUMOUNT -v $SRCDIR:$SRCMOUNT \
-	-it corroleaus/casper-ros:beta
+	-v $QEMUVOL:$QEMUMOUNT -v $VOL:$MOUNT -v $RESTVOL:$RESTMOUNT -v $CPPVOL:$CPPMOUNT  \
+	-v $DBDIR:$DBMOUNT --device="/dev/tty:$USBMOUNT" -it corroleaus/casper-ros:alpha
+	#docker exec -it --user builder rpi_ros_emulator bash -v /buildLidar.sh
+	#docker exec -it --user builder rpi_ros_emulator bash -v /runRest.sh
+	#docker exec -it --user builder rpi_ros_emulator bash -v /runSlam.sh
+	#docker exec -it --user builder rpi_ros_emulator bash -v /runVideo.sh
 fi
