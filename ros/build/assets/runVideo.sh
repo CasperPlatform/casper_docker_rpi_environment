a03cc48c8f1e4e57a9ddd0ee8a1e8b24935eb640
Linus Eiderström Swahn <es.linus@gmail.com>
2016-05-24 17:53:41 +0200

    Changed runVideo.sh

--- ros/build/assets/runVideo.sh
+++ ros/build/assets/runVideo.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-if [ ! -f /home/builder/src/cpp/build/videoserver ]; then
-    
-    cd /home/builder/src/cpp/videoserver && make
-
-    if [ ! -f /home/builder/src/cpp/build/videoserver ]; then
-            echo "build failed..." 2>&1
-            exit 1
-        fi	
-	
-    echo "driveserver built.. executing" >> /dev/stdout
-fi
-
 exec /home/builder/src/cpp/videoserver
