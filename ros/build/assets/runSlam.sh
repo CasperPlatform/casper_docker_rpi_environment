c9f71b5f850234932467ca929e2af14828759c1b
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-24 19:34:16 +0200

    fix

--- ros/build/assets/runSlam.sh
+++ ros/build/assets/runSlam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-source /opt/ros/indigo/setup.bash && source /envi.sh && roscore & \
-&& roslaunch hector_mapping mapping_default.launch 
+source /opt/ros/indigo/setup.bash && source /envi.sh && roscore & 
+roslaunch hector_mapping mapping_default.launch 
