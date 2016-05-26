c5f86b2eee13c636522339b2258929a75be2da21
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-26 19:31:47 +0200

    Added hector_slam in assets

--- ros/build/assets/runSlam.sh
+++ ros/build/assets/runSlam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/indigo/setup.bash && source /envi.sh \
-&& roslaunch -p 11311 hector_mapping mapping_default.launch
+&& roslaunch -p 11311 hector_slam_launch mapping_default.launch
  
